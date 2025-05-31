188d38a3fad88022914a3c8ccad57d467a1428df
higenyi benard <higenyibenard@gmail.com>
2025-05-31 12:17:35 +0100

    my fouth work alx

---
+++ future_age_calculator.py
@@ -0,0 +1,10 @@
+# future_age_calculator.py
+
+# Prompt the user to enter their current age
+current_age = 30
+
+# Calculate age in 2050 assuming the current year is 2023
+age_in_2050 = current_age + 27
+
+# Print the result
+print("In 2050, you will be ",age_in_2050)
--- hours_to_seconds.py
+++ hours_to_seconds.py
@@ -6,3 +6,4 @@ seconds = hours * 3600
 
 # Print the result in the specified format
 print(f"{hours} hour(s) is {seconds} seconds.")
+
